c15081d2f40036c3fb17d8f04888215e4df5b48a
Boris Dimitrov <95476370+BorisKipiDimitrov@users.noreply.github.com>
2023-07-20 17:34:31 +0300

    Fix collisions

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,15 +6,13 @@ const SPEED = 300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var _is_player_1 = true if get_meta("is_player_1") else false
+@onready var _is_player_1 = true if has_meta("is_player_1") else false
 @onready var _left_input = "player1left" if _is_player_1 else "player2left"
 @onready var _right_input = "player1right" if _is_player_1 else "player2right"
 @onready var _punch_input = "player1punch" if _is_player_1 else "player2punch"
 
 func _ready():
-	# Save layer 1 for the ground
-	set_collision_layer_value(2, _is_player_1)
-	set_collision_layer_value(3, not _is_player_1)
+	pass
 	
 
 func _physics_process(delta):
